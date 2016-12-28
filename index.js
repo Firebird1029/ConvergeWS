1457da64bb2edb361a617eec259e74c82aef82e6
Brandon Yee <byee20@punahou.edu>
2016-12-27 14:31:16 -1000

    Server Ready
    Turned off debug. Running on Daniel’s server.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict"; /* eslint-env node */ /* global */ /* eslint no-warning-comments: [1, { "terms": ["todo", "fix", "help"], "location": "anywhere" }] */
-var debug = true;
+var debug = false;
 
 // ADJUSTABLE - Change these variables if Airtable base structure changes (added fields, changed name of table, etc.)
 var apiKey = "keyFUzYF3ZYAosZWm", // https://airtable.com/account - TODO We need a universal API key.
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 "use strict"; /* eslint-env browser */ /* global */ /* eslint no-warning-comments: [1, { "terms": ["todo", "fix", "help"], "location": "anywhere" }] */
-var debug = true;
+var debug = false;
 
 $("#nav > ul").dropotron({
 	// mode: "fade",
