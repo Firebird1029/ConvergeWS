b3d08911b694378788550cf8458daafcf7a860fc
Brandon Yee <byee20@punahou.edu>
2017-01-22 23:26:21 -1000

    Disabled Debug

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 "use strict"; /* eslint-env browser */ /* global */ /* eslint no-warning-comments: [1, { "terms": ["todo", "fix", "help"], "location": "anywhere" }] */
-var debug = true;
+var debug = false;
 
 // Fix scroll glitch
 $(document).scrollTop(0);
