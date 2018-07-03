f1a3d595d7a4afae7ab42946e2ec384fa47496b6
Brandon Yee <brandon.yee150@gmail.com>
2018-07-02 20:01:32 -1000

    Reset Models

---
+++ models/resetModels.js
@@ -0,0 +1,12 @@
+var fs = require("fs"),
+	path = require("path"),
+	models = require("../routes/models.js");
+
+Object.keys(models.bases).forEach((base) => {
+	for (var i = 0; i < models.bases[base].tables.length; i++) {
+		fs.writeFile(path.join(__dirname, `../models/${base}_${ models.bases[base].tables[i] }.json`), "{}", 'utf8', function (err) {
+			if (err) { return console.log(err); }
+			console.log("All models reset to {}.");
+		});
+	}
+});
