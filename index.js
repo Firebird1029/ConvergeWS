855d581ff9830b95a190cbe7bcb125401d2b0779
Brandon <6111102+Firebird1029@users.noreply.github.com>
2021-02-21 18:36:46 -1000

    Format index.js

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-"use strict"; /* eslint-env node */ /* global */ /* eslint no-warning-comments: [1, { "terms": ["todo", "fix", "help"], "location": "anywhere" }] */
+"use strict" /* eslint-env node */ /* global */ /* eslint no-warning-comments: [1, { "terms": ["todo", "fix", "help"], "location": "anywhere" }] */;
 var debug = !process.env.NODE_ENV;
 console.log("Environment: ", process.env.NODE_ENV || "dev");
 console.log("Debugging: ", debug);
 
-/*
- * TODO
- *
- * Playground:
- * https://developers.google.com/speed/pagespeed/insights/
- * Calendar -- modal for Bible Ministry, https://postare.github.io/bulma-modal-fx/ (?)
- * Fuzzy string matching + TextRazor NLP API (?)
- * https://www.cloudflare.com/plans/
- */
-
 // Load Node Dependencies & Custom Modules
 var express = require("express"),
 	app = express(),
-	server = app.listen(process.env.PORT || (process.argv[2] || 8000), function expressServerListening () {
+	server = app.listen(process.env.PORT || process.argv[2] || 8000, function expressServerListening() {
 		debug && console.log(server.address());
 	}),
-
 	// Express Middleware
 	helmet = require("helmet"),
 	session = require("express-session"),
@@ -27,13 +16,11 @@ var express = require("express"),
 	cookieParser = require("cookie-parser"),
 	bodyParser = require("body-parser"),
 	csrf = require("csurf"),
-
 	// Project-Specific Dependencies
 	CronJob = require("cron").CronJob,
 	jsonfile = require("jsonfile"),
 	io = require("socket.io"),
 	listener = io.listen(server),
-
 	// Utilities & Custom Modules
 	_ = require("lodash"),
 	utils = require("./utils.js"),
@@ -52,8 +39,8 @@ app.use(
 );
 
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: false})); // Used for sending HTML form data within POST requests
-app.use(csrf({cookies: true})); // HTML form security
+app.use(bodyParser.urlencoded({ extended: false })); // Used for sending HTML form data within POST requests
+app.use(csrf({ cookies: true })); // HTML form security
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/dist"));
@@ -78,16 +65,24 @@ app.use((req, res, next) => {
 // 00 */2 * * * * -- every 2 hours
 // */6 * * * * * -- every 10 seconds
 // */12 * * * * * -- every 5 seconds
-var job = new CronJob("*/12 * * * * *", function () {
-	// Actual scanning happens in routes/models.js. This function simply transfers the pulled data into JSON files.
-	models.scanEveryTable(models.bases, function scanEveryTableCallback (data) {			
-		Object.keys(data).forEach(function processFullDataCallback (key) {
-			jsonfile.writeFile(`${__dirname}/models/${key}.json`, data[key], function jsonWriteFileCallback (err) {
-				if (debug && err) { throw new Error(err); }
+var job = new CronJob(
+	"*/12 * * * * *",
+	function () {
+		// Actual scanning happens in routes/models.js. This function simply transfers the pulled data into JSON files.
+		models.scanEveryTable(models.bases, function scanEveryTableCallback(data) {
+			Object.keys(data).forEach(function processFullDataCallback(key) {
+				jsonfile.writeFile(`${__dirname}/models/${key}.json`, data[key], function jsonWriteFileCallback(err) {
+					if (debug && err) {
+						throw new Error(err);
+					}
+				});
 			});
+			// debug && console.log(`Cron job successful! Files ${__dirname}/models/*.json updated.`);
 		});
-		// debug && console.log(`Cron job successful! Files ${__dirname}/models/*.json updated.`);
-	});
-}, function () {
-	debug && console.log("Cron job stopped.");
-}, true, "Pacific/Honolulu");
+	},
+	function () {
+		debug && console.log("Cron job stopped.");
+	},
+	true,
+	"Pacific/Honolulu"
+);
